3dccf23dfaf8084d4e1108f3cff1201b2a774516
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2020-08-24 22:01:25 -0300

    ci - organizating directories by degrees

---
+++ review/functions/aula01.js
@@ -0,0 +1,25 @@
+hello();
+
+
+function hello(){
+  console.log('Hellow World');
+}
+
+const dice = () =>{
+  console.log('dice')
+}
+
+function executaFuncao(funcao){
+  console.log('Executando uma função por parâmetro.');
+  funcao();
+
+}
+
+executaFuncao(dice);
+
+//Arrows function
+const arrow = ()=>{
+  console.log('Criando uma arrow function. Aqui não é permitido usar o \'this\''); 
+}
+
+arrow();
